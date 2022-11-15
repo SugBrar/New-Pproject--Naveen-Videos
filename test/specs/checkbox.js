0c54e6a1f24cb4505e77dbdccf7c9cc607cb80e8
SugBrar <brar.sugreev@gmail.com>
2022-11-15 13:57:59 -0500

    clicking on checkboxes

---
+++ test/specs/checkbox.js
@@ -0,0 +1,42 @@
+const InternetPage = require('../pageobjects/checkBoxHandle')
+const assert = require("assert")
+
+describe("Handle CheckBox", function(){
+
+    it("Check page Url", async ()=>{
+
+        await browser.url('https://the-internet.herokuapp.com/')
+        await browser.pause(2000)
+        
+        await expect(browser).toHaveUrlContaining("the-internet")
+
+    })
+
+    it("Check checkbox page header",async () =>{
+        await browser.url('https://the-internet.herokuapp.com/')
+        await InternetPage.clickOnLink(6)
+        await browser.pause(2000)
+        
+        
+        await console.log("Title :  ", browser.getTitle())
+
+    })
+
+    it("Should click checkbox", async function(){
+
+        await browser.url('https://the-internet.herokuapp.com/')
+        await browser.pause(2000)
+
+        await InternetPage.clickOnLink(6)
+        await browser.pause(2000)
+
+        await InternetPage.h3Header.waitForDisplayed()
+
+        await InternetPage.clickOnCheckBox(1)
+        await browser.pause(2000)
+        await expect(InternetPage.getCheckBoxElement(1)).toBeSelected()
+        //await assert.equal(await InternetPage.getCheckBoxElement(1).isSelected(),true)
+        await browser.pause(2000)
+
+    })
+})
