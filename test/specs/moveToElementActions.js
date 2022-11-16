b6a151b65409533acb59b90f5ed29ee21ed26055
SugBrar <brar.sugreev@gmail.com>
2022-11-16 13:48:39 -0500

    keys action

--- test/specs/moveToElementActions.js
+++ test/specs/moveToElementActions.js
@@ -1,26 +1,33 @@
 //const cartersPage = require('../pageobjects/moveToElement')
-const amazonPage = require('../pageobjects/moveToElementAm')
+//const amazonPage = require('../pageobjects/moveToElementAm')
+const actionPage = require('../pageobjects/actions')
 
 describe("Page action features",  () =>{
 
-    it("Move to Element",async() =>{
+    // it("Move to Element",async() =>{
 
-        //browser.url('https://www.cartersoshkosh.ca/')
-        browser.url('https://www.amazon.ca/b?ie=UTF8&node=16040885011')
-        await browser.maximizeWindow()
-        await browser.pause(2000)
+    //     //browser.url('https://www.cartersoshkosh.ca/')
+    //     browser.url('https://www.amazon.ca/b?ie=UTF8&node=16040885011')
+    //     await browser.maximizeWindow()
+    //     await browser.pause(2000)
 
-        // await cartersPage.moveToElement(await cartersPage.baby)
-        // await cartersPage.moveToElement(await cartersPage.babyGirl)
-        // await cartersPage.clickElement(await cartersPage.babyGirlOptions)
-        await amazonPage.moveToElement(await amazonPage.Account)
-        await amazonPage.clickElement(await amazonPage.Orders)
-        await browser.pause(2000)
+    //     // await cartersPage.moveToElement(await cartersPage.baby)
+    //     // await cartersPage.moveToElement(await cartersPage.babyGirl)
+    //     // await cartersPage.clickElement(await cartersPage.babyGirlOptions)
+    //     await amazonPage.moveToElement(await amazonPage.Account)
+    //     await amazonPage.clickElement(await amazonPage.Orders)
+    //     await browser.pause(2000)
 
         
-    })
+    // })
 
-    it("Keys Action", async() =>{
-        
+    it("Keys Action perform Tab", async() =>{
+
+        browser.url('https://the-internet.herokuapp.com/key_presses?')
+        await actionPage.enterSearch("Escape")
+        const ent = await actionPage.getResultLabelText()
+        console.log(ent)
+        await browser.pause(2000)
+
     })
 })
